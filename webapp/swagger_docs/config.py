7f3475fdfba7aa503b867d663074aa7e1a7a5322
owen-eternal <olwethuphakade@gmail.com>
2021-08-30 10:02:22 +0200

    removed the swaggerUI spec because of a bug

--- webapp/swagger_docs/config.py
+++ webapp/swagger_docs/config.py
@@ -2,7 +2,7 @@ template = {
   "swagger": "2.0",
   "info": {
     "title": "Sorting Algorithm",
-    "description": "A simple sorting API, made with Python.",
+    "description": "See the full documentation on: https://github.com/owen-eternal/sorting#readme",
     "contact": {
       "responsibleDeveloper": "Owen Phakade",
       "email": "olwethuphakade89@gmail.com",
@@ -10,10 +10,6 @@ template = {
     "version": "0.0.1"
   },
   "basePath": "/api/v1", 
-  "schemes": [
-    "http",
-    "https"
-  ],
   "operationId": "getmyData"
 }
 
@@ -31,5 +27,5 @@ swagger_config = {
     "static_url_path": "/flasgger_static",
     # "static_folder": "static",  # must be set by user
     "swagger_ui": True,
-    "specs_route": "/apidocs"
+    "specs_route": "/"
 }
